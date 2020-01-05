4c8ccb33a31e3dcad056097a02908c115a865109
Magdalena Piłat <magdalena.pilat@schibsted.com>
2020-01-05 19:30:54 +0100

    Fix vertical calendar

--- src/components/VerticalCalendar/VerticalCalendar.jsx
+++ src/components/VerticalCalendar/VerticalCalendar.jsx
@@ -33,6 +33,9 @@ const VerticalCalendar = () => {
     const [displayName, setDisplayName] = useState('');
     const [calendarEvents, setCalendarEvents] = useState();
     const [calendarEventsDates, setCalendarEventsDates] = useState([]);
+    const [eventTitle, setEventTitle] = useState('');
+    const [eventDescription, setEventDescription] = useState('');
+
     const exampleDates = [new Date('Nov 3, 2019'), new Date('Nov 15, 2019'), new Date('Dec 15, 2019')];
   
     firebase.auth().onAuthStateChanged(user => {
@@ -79,8 +82,13 @@ const VerticalCalendar = () => {
     
     const onDayClick = day => {
         setClickedDay(day);
-        if (areDaysEqual(day, exampleDates, currentDate.getMonth())){
-            setIsEventExpanded(true);
+        if (areDaysEqual(day, calendarEventsDates, currentDate.getMonth())){
+          
+          const calendarEventItem = calendarEvents.find(calendarEvent => calendarEvent.date.seconds === Math.floor(new Date(2020, currentDate.getMonth(), day).getTime() / 1000));
+          setEventTitle(calendarEventItem.title);
+          setEventDescription(calendarEventItem.description);
+          setIsEventExpanded(true);
+
         } else {
             setIsEventExpanded(false);
 
@@ -95,7 +103,7 @@ const VerticalCalendar = () => {
 
 return (
          <div className={css.container}>
-             <CalendarEvent key={isEventExpanded} date={date} isEventExpanded={isEventExpanded} className={css.calendarEvent}/>
+             <CalendarEvent key={isEventExpanded} date={date} title={eventTitle} description={eventDescription} isEventExpanded={isEventExpanded} className={css.calendarEvent}/>
              <span className={css.days}>
                 {days.map(day => <div key={day} className={cln('day',
                     {
